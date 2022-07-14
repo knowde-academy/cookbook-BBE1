efe6ab6ca94ea66ca66b7e03ad5c68cf4d9453ac
Daniel Sobolewski <daniel14031996@wp.pl>
2022-07-14 06:35:50 +0000

    Add cooking_time field to recipes model, add validation & response
    Add cooking_time field to recipes model, add validation & response

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,9 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :content
+  attributes :id, :name, :content, :cooking_time
+  
+  def cooking_time
+    return "unknow" unless object.cooking_time
+    
+    object.cooking_time
+  end
 end

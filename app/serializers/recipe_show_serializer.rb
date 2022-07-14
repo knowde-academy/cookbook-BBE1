cca03ffc297043ce84832704653ec932f645239a
Ewa Krzysztoporska <e.krzysztoporska@o2.pl>
2022-07-14 10:34:07 +0000

    add spec to recipe_show_serialize and rubocop

--- app/serializers/recipe_show_serializer.rb
+++ app/serializers/recipe_show_serializer.rb
@@ -1,10 +1,9 @@
 class RecipeShowSerializer < RecipeSerializer
   attributes :price
-  
+
   def price
-    return "unknown" unless object.price
-    
+    return 'unknown' unless object.price
+
     object.price
-    
   end
 end
---
+++ spec/serializers/recipe_show_serializer_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe RecipeShowSerializer do
+  subject(:serialized_recipe) { described_class.new(recipe).to_h }
+
+  let(:recipe) { build(:recipe) }
+
+  it { is_expected.to include(name: recipe.name) }
+  it { is_expected.to include(content: recipe.content) }
+
+  describe '#price' do
+    context 'with price ' do
+      it { is_expected.to include(price: recipe.price) }
+    end
+
+    context 'without price ' do
+      let(:recipe) { build(:recipe, price: nil) }
+
+      it { is_expected.to include(price: 'unknown') }
+    end
+  end
+end

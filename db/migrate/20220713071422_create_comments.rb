acadbb85b722f59b53ec48133a7bff2b2ca91547
Ewa Krzysztoporska <e.krzysztoporska@o2.pl>
2022-07-15 09:55:38 +0000

    remove comment for Rogal

--- db/migrate/20220713071422_create_comments.rb
+++ db/migrate/20220713071422_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.string :author #dodajemu autora , jesli go nie ma "unnown"
+      t.string :author 
       t.string :body, null: false
       t.belongs_to :recipe, foreign_key: true
       

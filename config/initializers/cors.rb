a326ff16557d07d27d3bfeefeb8f6455466e0f8a
Daniel Sobolewski <daniel14031996@wp.pl>
2022-07-17 14:58:13 +0000

    edi migration, add cors, add request spec and user factories

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ENV.fetch('API_ALLOWED_ORIGINS', '*').to_s.split(',')
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: false
-  end
+Rails.application.config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          methods: [:get, :post, :options, :delete, :put]
+      end
 end
+
+

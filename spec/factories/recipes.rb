6dee12270171216b23a70892686b201542fcbe67
Ewa Krzysztoporska <e.krzysztoporska@o2.pl>
2022-07-18 09:08:11 +0000

    add Faker for levelin facotries

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     cooking_time { Faker::Number.number(digits: 2) }
     price { Faker::Number.number(digits: 2) }
     video_link { Faker::Internet.url }
+    level { Faker::Number.within(range: 1..5)}
   end
 end

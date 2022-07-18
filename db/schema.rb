d5f9761711a198d854998c0110684e0fbccdaa7e
Ewa Krzysztoporska <e.krzysztoporska@o2.pl>
2022-07-18 08:40:22 +0000

    fix validation for author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_113153) do
+ActiveRecord::Schema.define(version: 2022_07_15_111551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 2022_07_14_113153) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "cooking_time"
     t.decimal "price", precision: 7, scale: 2
+    t.integer "cooking_time"
+    t.integer "level"
     t.string "video_link"
   end
 

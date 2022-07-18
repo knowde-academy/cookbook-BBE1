c7373ce65739944f7b537616ca8e90289fec2d9c
Daniel Sobolewski <92857028+dansob14@users.noreply.github.com>
2022-07-18 20:57:09 +0200

    [BE-08] add sign in/sign up (#5)
    * install devise-auth gem
    
    * remove disable forgery protection for JSON requests
    
    * configure devise_token_auth
    
    * remove before_action authentication after tests in recipes_controller
    
    * edi migration, add cors, add request spec and user factories
    
    * add sign_up and sign_in rspec, add user factories, edit auth pathin routes, use rubocop
    
    * remeve unused modules in user model, remove unused confings on rails_helper, add spec to sign in and sign up with invalid params
    
    * correction after cr

---
+++ spec/requests/sign_in_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe '#sign_in' do
+  let(:user) { create(:user) }
+  let(:login_params) do
+    {
+      email: user.email,
+      password: user.password
+    }
+  end
+  let(:login_user) do
+      post '/api/v1/auth/sign_in', params: login_params
+  end
+  
+  context 'with valid params' do
+    it 'request should be successful' do
+      login_user
+      expect(response).to have_http_status(:ok)
+    end
+    
+    it 'check auth headers' do
+      login_user
+      expect(response.headers['uid']).to be_present
+      expect(response.headers['client']).to be_present
+      expect(response.headers['access-token']).to be_present
+    end
+  end
+  
+  context 'with invalid params' do
+    let!(:login_params) { { password: 'InvalidPassword' } }
+    
+    it 'request should be unauthorized' do
+      login_user
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+end
